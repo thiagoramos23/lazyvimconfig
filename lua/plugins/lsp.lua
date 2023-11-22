042ae103c8eb4d1cff3328e3e9507912f3ec9e81
Thiago Ramos <thiagoramos.al@gmail.com>
2023-11-22 20:54:36 -0300

    Add lexical

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
       servers = {
         lexical = {
           filetypes = { "elixir", "eelixir" },
+          cmd = { "/Users/thiago/personal/open_source/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
         },
       },
     },
