089bf33fb027eaae14da7acca6565aaa943506ee
Thiago Ramos <thiagoramos.al@gmail.com>
2023-11-22 00:05:57 -0300

    Develop

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      "bash",
+      "html",
+      "elixir",
+      "erlang",
+      "heex",
+      "eex",
+      "lua",
+      "javascript",
+      "json",
+      "markdown",
+      "markdown_inline",
+      "vim",
+      "yaml",
+      "tsx",
+      "typescript",
+    },
+  },
+}
