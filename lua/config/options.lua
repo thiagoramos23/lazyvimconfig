f56f0c6f47262fea82b81c77891fc26f7a41007f
Thiago Ramos <thiagoramos.al@gmail.com>
2023-11-22 00:14:11 -0300

    Keep default leader

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local g = vim.g
-g.mapleader = ","
 
 -- Vim Test
 g["rspec_runner"] = "os_x_iterm2"
