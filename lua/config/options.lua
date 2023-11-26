6329aee80d269a63e6e0c2ccbbe4216f6bac1020
Thiago Ramos <thiagoramos.al@gmail.com>
2023-11-26 20:17:47 -0300

    Changing mapleader

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local g = vim.g
+g.mapleader = ","
 
 -- Vim Test
 g["rspec_runner"] = "os_x_iterm2"
 g["test#strategy"] = "vimux"
 g["VimuxHeight"] = "30"
+
+vim.opt.foldlevel = 3
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "codeium" })
+    end,
+  },
+}
