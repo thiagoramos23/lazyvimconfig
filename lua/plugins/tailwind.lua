27fe5893391210004af3805d8f6c7cd604ed3886
Thiago Ramos <thiagoramos.al@gmail.com>
2023-11-27 23:50:08 -0300

    Fix tailwind css for .ex files and updated other plugins

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -3,8 +3,49 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        tailwindcss = {},
+        tailwindcss = {
+          init_options = {
+            userLanguages = {
+              elixir = "phoenix-heex",
+              heex = "phoenix-heex",
+            },
+          },
+          includeLanguages = {
+            ["html-eex"] = "html",
+            ["phoenix-heex"] = "html",
+            heex = "html",
+            eelixir = "html",
+            elixir = "html",
+          },
+          -- filetypes_include = { "eruby", "eelixir", "elixir", "heex", "html", "html-heex" },
+        },
       },
+      -- setup = {
+      --   tailwindcss = function(_, opts)
+      --     local tw = require("lspconfig.server_configurations.tailwindcss")
+      --     opts.filetypes = opts.filetypes or {}
+      --     opts.init_options = {
+      --       userLanguages = {
+      --         elixir = "phoenix-heex",
+      --         eruby = "erb",
+      --         heex = "phoenix-heex",
+      --         svelte = "html",
+      --         rust = "html",
+      --       },
+      --     }
+      --     -- Add default filetypes
+      --     vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+      --
+      --     -- Remove excluded filetypes
+      --     --- @param ft string
+      --     opts.filetypes = vim.tbl_filter(function(ft)
+      --       return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+      --     end, opts.filetypes)
+      --
+      --     -- Add additional filetypes
+      --     vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+      --   end,
+      -- },
     },
   },
   {
