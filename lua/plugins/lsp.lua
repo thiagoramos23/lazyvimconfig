7ece10fb1f1b3a2a05ea3144e9aef42e5be25f39
Thiago Ramos <thiagoramos.al@gmail.com>
2024-02-26 19:53:02 -0300

    do not use codeium or lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,5 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        lexical = {
-          filetypes = { "elixir", "eelixir" },
-          cmd = {
-            "/Users/quaseninja/projects/open_source/lexical/_build/dev/package/lexical/bin/start_lexical.sh",
-          },
-        },
-      },
-    },
   },
 }
